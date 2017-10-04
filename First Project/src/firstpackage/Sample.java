007b5a4db9d1f7a7683052e1bcb95abfb6c9390d
Manish <meantforjob@gmail.com>
2017-10-03 21:29:27 -0400

    FirstCommit

---
+++ First Project/src/firstpackage/Sample.java
@@ -0,0 +1,41 @@
+package firstpackage;
+
+import java.io.Console;
+import java.lang.*;
+
+public class Sample {
+	
+	/*private static int a;
+	//Creating a method
+	public int addition(int a,int b,int c){
+		int result = a * b * c;
+				return result;
+			
+	}
+public static void main(String[]args) {
+	boolean x = true, y = false;
+	System.out.println(!(!x || !y));
+	
+	System.out.println ("Hello Manish, you rock the world!");
+	Sample obj = new Sample();
+	int finalresult = obj.addition(1, 4, 7);
+	
+	System.out.println("addition is " + finalresult);
+	// loop
+		for (int za = 1;za >=1 ;za++){
+			System.out.println(za);
+	
+	}*/
+	//}
+		//Sample class bracket
+
+public static void main(String[]args) {
+	String [] languages = {"a"};
+for (String lang : languages){
+System.out.println(lang);
+
+}
+}
+}
+
+
